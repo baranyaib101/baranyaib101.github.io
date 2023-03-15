7ef70473209cd8a1c7999de986900fe9a4bad014
baranyaib101 <bbaranyai@yahoo.com>
2023-03-15 18:00:42 +0100

    navi

---
+++ script.js
@@ -0,0 +1,12 @@
+// const yearEl = document.querySelector(".year");
+// const currentYear = new Date().getFullYear();
+// yearEl.textContent = currentYear;
+
+const checkbox = document.querySelector(".navigation__checkbox");
+const navlinkArray = document.querySelectorAll(".navigation__link");
+
+navlinkArray.forEach((elem) =>
+  elem.addEventListener("click", function () {
+    checkbox.checked = false;
+  })
+);
