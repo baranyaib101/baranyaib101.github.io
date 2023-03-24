f6160ab300d10d940eaf682e748bbb6abbe50dff
baranyaib101 <bbaranyai@yahoo.com>
2023-03-24 19:46:02 +0100

    Navigation and Business Card new version

--- script.js
+++ script.js
@@ -2,32 +2,41 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-const checkbox_nav = document.querySelector(".navigation__checkbox");
-const navlinkArray = document.querySelectorAll(".navigation__link");
+// NAVIGATION
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const btnbcEl = document.querySelector(".btn-mobile-bc");
+const navEl = document.querySelector(".navigation");
+const bcEl = document.querySelector(".businesscard");
+
+btnNavEl.addEventListener("click", function () {
+  navEl.classList.toggle("nav-open");
+  if (bcEl.classList.contains("bc-open")) {
+    bcEl.classList.toggle("bc-open");
+  }
+});
+
+const navlinkArray = document.querySelectorAll(".main-nav-link");
 
 navlinkArray.forEach((elem) =>
   elem.addEventListener("click", function () {
-    checkbox_nav.checked = false;
+    navEl.classList.toggle("nav-open");
   })
 );
 
-const checkbox_bc = document.querySelector(".businesscard__checkbox");
-const bclinkArray = document.querySelectorAll(".businesscard__link");
+// CONTACT INFO
+
+btnbcEl.addEventListener("click", function () {
+  bcEl.classList.toggle("bc-open");
+  if (navEl.classList.contains("nav-open")) {
+    navEl.classList.toggle("nav-open");
+  }
+});
+
+const bclinkArray = document.querySelectorAll(".businesscard-link");
 
 bclinkArray.forEach((elem) =>
   elem.addEventListener("click", function () {
-    checkbox_bc.checked = false;
+    bcEl.classList.toggle("bc-open");
   })
 );
-
-checkbox_nav.addEventListener("click", function () {
-  if ((checkbox_nav.checked === true) & (checkbox_bc.checked === true)) {
-    checkbox_bc.checked = false;
-  }
-});
-
-checkbox_bc.addEventListener("click", function () {
-  if ((checkbox_bc.checked === true) & (checkbox_nav.checked === true)) {
-    checkbox_nav.checked = false;
-  }
-});
